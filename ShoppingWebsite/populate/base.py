267fe161a159558c038b394f3c7007928787601a
haru3613 <qaw8975889@gmail>
2018-01-23 09:48:08 -0800

    452452

--- ShoppingWebsite/populate/base.py
+++ ShoppingWebsite/populate/base.py
@@ -1,4 +1,4 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShoppingWebsite.settings')
 import django
 django.setup()
